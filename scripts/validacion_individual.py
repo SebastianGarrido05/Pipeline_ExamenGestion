07048d324ef42b4556adf7d733bb335907b9322f
SebastianGarrido05 <se.garridom@duocuc.cl>
2026-07-04 19:27:54 -0400

    Se crea la estructura base para el trabajo individual y en grupo y se carga el raw

---
+++ scripts/validacion_individual.py
@@ -0,0 +1,47 @@
+# id_pedido
+
+
+
+# Fecha
+
+
+
+# Rut cliente
+
+
+
+# Nombre cliente
+
+
+
+# Region
+
+
+
+# Producto
+
+
+
+# Cateogria
+
+
+
+# Cantiodad
+
+
+
+# Precio unitario
+
+
+
+# Descuento
+
+
+
+# Estado pedido
+
+
+
+# Fecha despacho
+
+
